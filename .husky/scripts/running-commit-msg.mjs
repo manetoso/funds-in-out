1a98ac5e508d82d9b887a67ef56541b46904597b
manetoso <manetoso98@hotmail.com>
2024-10-19 11:42:05 -0600

    feat: add repo setup
    - Add linter
    
    - Add prettier
    
    - Add commitlint
    
    - Add husky

---
+++ .husky/scripts/running-commit-msg.mjs
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+const magenta = chalk.bgHex("#342136").hex("#f197f7").bold;
+
+console.log(magenta("\n🔮 Runnig commit message evaluation 🔮\n"));
---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "revert"],
+    ],
+  },
+};
